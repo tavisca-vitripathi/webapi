fc2957c4f8556c46de62feea2e7fe8b52ba0d884
Tripathi <vitripathi@internal.tavisca.com>
2019-08-14 12:34:23 +0530

    first commit

---
+++ webapi/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers
+{
+    [Route("api/[Controller]/[Action]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+       
+        [HttpGet]
+        public  string greeting(string str)
+        {   if(str=="hello")
+             return "hi";
+            else
+            {
+                return "invalid";
+            }
+        }
+    }
+}
